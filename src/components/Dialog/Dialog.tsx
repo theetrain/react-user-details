5ed83146967fb7880397efe14ca57bf6368137f5
Enrico Sacchetti <enrico@theetrain.ca>
2025-07-09 21:33:20 -0400

    fix: dark mode

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -41,6 +41,17 @@ const Dialog = forwardRef<DialogHandle, DialogProps>(
             className={`${styles.dialogClose} row`}
             onClick={closeDialog}
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+            >
+              <path
+                fill="currentColor"
+                d="M17.414 16L24 9.414L22.586 8L16 14.586L9.414 8L8 9.414L14.586 16L8 22.586L9.414 24L16 17.414L22.586 24L24 22.586z"
+              />
+            </svg>{' '}
             {closeLabel || 'Close'}
           </button>
           {children}
